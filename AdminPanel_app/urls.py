9fbd80ad11cf97e1f8ddd51a950a59e0552ccaf0
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-20 19:01:22 +0430

    finish

--- AdminPanel_app/urls.py
+++ AdminPanel_app/urls.py
@@ -6,7 +6,9 @@ urlpatterns=[
     path("",views.AdminPanelView.as_view(),name="admin_panel"),
     path("drafts",views.Drafts.as_view(),name="admin_panel_drafts"),
     path("create_video/",views.CreateVideo.as_view(),name="create_video"),
-    re_path(r'edit/(?P<slug>[-\w]+)/', views.EditVideo.as_view(), name="edit_video"),
-    re_path(r'delet/(?P<slug>[-\w]+)/', views.DeletVideo.as_view(), name="delet_video"),
+    # re_path(r'edit/(?P<slug>[-\w]+)/', views.EditVideo.as_view(), name="edit_video"),
+    path("edit/<int:pk>/",views.EditVideo.as_view(),name="edit_video"),
+    # re_path(r'delet/(?P<slug>[-\w]+)/', views.DeletVideo.as_view(), name="delet_video"),
+    path("delet/<int:pk>/",views.DeletVideo.as_view(),name="delet_video"),
     path("message/",views.MessageView.as_view(),name="message")
 ]
